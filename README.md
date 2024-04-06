60780f75cc478ed87a15ca431f8f37bba51a5575
nandinigummalla <nandinigummalla27@gmail.com>
2024-04-07 00:45:33 +0530

    episode3 part 3

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 #Namasthey React
+
+npm start- to run the code
+npm build - to build the production build
--- app.js
+++ app.js
@@ -3,6 +3,15 @@ import ReactDOM from "react-dom/client";
 
 // createElement will give a JS object
 const heading = React.createElement("h1", { id: "head" }, "Learning React");
+
+//jsx will be html like code but not html ( it will be an js object at the end )
+//jsx will convert to React.createElement using babel transpilation 
+const jsxheading = (<h1 className="head">
+    Learning React using JSX
+    </h1>)
+
+const imgTag = <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fbuffer.com%2Flibrary%2Ffree-images%2F&psig=AOvVaw0VJvFoBTrg66LSJK6BxUcJ&ust=1712516703967000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMiYh4mkroUDFQAAAAAdAAAAABAJ" alt="image"/>
+const aTag = <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/script">click me</a>
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // once we render the object will be converted to a html tag
-root.render(heading);
+root.render(jsxheading);
