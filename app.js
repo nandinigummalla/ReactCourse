2efc48ec8ccbbc858e2a103cfd790859fc893b71
nandinigummalla <nandinigummalla27@gmail.com>
2024-04-06 14:22:24 +0530

    3rd episode 1st part

--- app.js
+++ app.js
@@ -1,24 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-{/* <div id="parent">
-<div id="child">
-    <h1>I am h1 tag</h1>
-    <h2>I am h2 tag</h2>
-</div>
-</div> */}
 
-
-const parent = React.createElement(
-    "div",
-    { id: "parent" },
-    React.createElement(
-        "div",
-        { id: "child" },
-        [React.createElement("h1", {}, "I am an h1 tag"),
-        React.createElement("h2", {}, "I am an h2 tag")]
-    ));
-
-const heading = React.createElement("h1", { id: "heading" }, "Hi this is from React app.jsfile");
-console.log(heading);
+// createElement will give a JS object
+const heading = React.createElement("h1", { id: "head" }, "Learning React");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// once we render the object will be converted to a html tag
+root.render(heading);
