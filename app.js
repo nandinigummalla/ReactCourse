3d9b0757afd316f6b05c1e7ac9dcdeb03fa24871
nandinigummalla <nandinigummalla27@gmail.com>
2024-04-07 07:05:16 +0530

    ep3 part5

--- app.js
+++ app.js
@@ -15,13 +15,25 @@ const OtherHeadingFunction = () => <h1>hello</h1>;
 const Title = () => <h1> Title </h1>;
 
 // Component in Component is component composition
+// we can call a functioncomponent as a normal function also
+// all 3 calling of title are same
 const MainHeading = () => (
   <div>
+    {Title()}
     <Title />
+    <Title></Title>
     <h2>Component composition</h2>
   </div>
 );
 
+// React element in react element
+const headElement = <h1>Hello I am react element</h1>;
+const mainHead = (
+  <div>
+    Hie I aam MainHeading
+    {headElement}
+  </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // for rendering component need to specify in </>
 root.render(<MainHeading />);
